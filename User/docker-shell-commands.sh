2924012a4db7c0e002c8c91e23adde8f00640253
lasalhettiarachchi <lasalshettiarachchi458@gmail.com>
2025-03-06 16:18:41 +0530

    [HMS - 003]: Docker files and build pack configurations

---
+++ User/docker-shell-commands.sh
@@ -0,0 +1,7 @@
+docker build --platform=linux/amd64 . -t lasalhettiarachchi/userms:s2
+# provides all the images in the system
+docker images
+docker run -p 8080:8080 lasalhettiarachchi/userms:s2
+# aab is the image id, provides the details of the image by invoking docker image
+docker inspect image aab
+docker push docker.io/lasalhettiarachchi/userms:s2
